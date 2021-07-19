f173e808a20df57d6b49022f69f23cc1c49c4874
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2021-07-19 16:28:43 -0300

    support

---
+++ funcs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function msg() {
+  printf "$(tput setaf 2)[ INFO ]$(tput sgr 0) $*\n"
+}
+
+function get_latest_tag() {
+  printf "$(curl -s "https://api.github.com/repos/binance-chain/bsc/tags" | jq -r '.[0].name')"
+}
+
+function get_latest_release() {
+  curl -sL https://api.github.com/repos/binance-chain/bsc/releases/latest | jq -r '.assets[2].browser_download_url' | wget -i -
+}
+
+function get_clone_repo() {
+  git clone -q https://github.com/binance-chain/bsc
+}
