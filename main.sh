16d56b2f54b0d48bc18255608496b04f3631c843
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2021-07-19 21:26:25 -0300

    load version funcs.conf

--- main.sh
+++ main.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
 . funcs.conf
-version=$(grep "^ID=" /etc/os-release | awk -F '=' '{print $2}')
 
-if [ $version = "ubuntu" ] || [ $version = "debian" ]; then
-	(msg "updating environment..."
+if [ $(get_version) = "ubuntu" ] || [ $(get_version) = "debian" ]; then
+	(get_msg "updating environment..."
 	apt update -qq && \
 	apt list --upgradable && \
 	apt upgrade -y
 	
-	msg "installing packages..."
+	get_msg "installing packages..."
 	apt-get install -y build-essential python3-venv hdparm \
                            python3-pip libssl-dev unzip netcat \
                            python3-dev libffi-dev golang nmap \
                            inxi vim git tmux htop jq
 	
-	msg "setting paths..."
+	get_msg "setting paths..."
 	get_clone_repo
 	cd bsc && make geth
 	
-	msg "get mainnet $(get_latest_tag)"
+	get_msg "get mainnet $(get_latest_tag)"
 	get_latest_release
 	unzip -q mainnet.zip && rm -f $_
 	
-	msg "running node..."
+	get_msg "running node..."
 	tmux new -d -s fullnode
 	tmux send-keys -t fullnode.0 './build/bin/geth --datadir node init genesis.json' ENTER
 	tmux send-keys -t fullnode.0 './build/bin/geth --config ./config.toml --datadir ./node --cache 18000 --rpc.allow-unprotected-txs --txlookuplimit 0' ENTER) | tee install.log
