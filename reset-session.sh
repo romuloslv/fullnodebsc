1cd679ab86d682af233c2fef2e14a5d3910c61e3
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2021-07-27 23:17:08 -0300

    remove local

--- reset-session.sh
+++ reset-session.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-local DIR="$(find / -iname config.toml | cut -d '/' -f 1-4)"
-local SESSION="$(tmux ls | awk -F ':' '{print $1}')"
+DIR="$(find / -iname config.toml | cut -d '/' -f 1-4)"
+SESSION="$(tmux ls | awk -F ':' '{print $1}')"
 
 tmux kill-session -t $SESSION && tmux new -d -s $SESSION
 tmux send-keys -t $SESSION.0 '$DIR/build/bin/./geth --config $DIR/config.toml --datadir $DIR/node --cache 18000 --rpc.allow-unprotected-txs --txlookuplimit 0' ENTER
