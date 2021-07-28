b1fff93d57d3037b5a62b8bc4c8c6a6c5586c7b3
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2021-07-28 01:04:16 -0300

    fix memory buffer

--- reset-session.sh
+++ reset-session.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-DIR="$(find / -iname config.toml 2>/dev/null | cut -d '/' -f 1-4)"
-SESSION="$(tmux ls | awk -F ':' '{print $1}')"
+sync; echo 3 > /proc/sys/vm/drop_caches
+
+export DIR="$(find / -iname config.toml 2>/dev/null | cut -d '/' -f 1-4)"
+export SESSION="$(tmux ls | awk -F ':' '{print $1}')"
 
 tmux kill-session -t $SESSION && tmux new -d -s $SESSION
 tmux send-keys -t $SESSION.0 '$DIR/build/bin/./geth --config $DIR/config.toml --datadir $DIR/node --cache 18000 --rpc.allow-unprotected-txs --txlookuplimit 0' ENTER
+printf "Session reseted...\n"
