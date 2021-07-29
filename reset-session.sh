6f91f1cd661f4755854928103d3f84b598f5aca6
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2021-07-29 05:52:11 -0300

    add default session

--- reset-session.sh
+++ reset-session.sh
@@ -5,6 +5,8 @@ sync; echo 3 > /proc/sys/vm/drop_caches
 export DIR="$(find / -iname config.toml 2>/dev/null | cut -d '/' -f 1-4)"
 export SESSION="$(tmux ls | awk -F ':' '{print $1}')"
 
+[ -z $SESSION ] && tmux new -d -s fullnode
+
 tmux kill-session -t $SESSION && tmux new -d -s $SESSION
 tmux send-keys -t $SESSION.0 '$DIR/build/bin/./geth --config $DIR/config.toml --datadir $DIR/node --cache 18000 --rpc.allow-unprotected-txs --txlookuplimit 0' ENTER
 printf "Session reseted...\n"
